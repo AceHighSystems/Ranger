42a27e498bc1dd3769090af5d5675ca1fe5f6348
Tor Kaufmann Gjerde <tor@Tors-MacBook-Pro.local>
2026-05-08 18:46:35 +0200

    added error handling to python firmware programmer

--- Python/host/RangerCAN.py
+++ Python/host/RangerCAN.py
@@ -4,7 +4,8 @@ import time
 import threading
 import can
 import argparse
-
+import queue
+import ace_constants as ACE
 # -----------------------------
 # Ace protocol / Ranger constants
 # -----------------------------
@@ -36,17 +37,13 @@ RANGER_PARAM_NODE_ID          = 0x10   # parameter ID for Node variable
 
 # Status codes - specific for Ace protocol
 # Valid status codes for response frame sent after reception of a command frame from host
-STATUS_OK              = 0x10
-STATUS_QUEUED          = 0x11
-STATUS_DATA_FOLLOWS    = 0x12
-STATUS_UNKNOWN_COMMAND = 0x13
-STATUS_INVALID_PARAM   = 0x14
+ACE_STATUS_OK           = 0x10
+ACE_STATUS_QUEUED       = 0x11
+ACE_STATUS_DATA_FOLLOWS = 0x12
+ACE_STATUS_UNKNOWN_COMMAND  = 0x13
+ACE_STATUS_UNKNOWN_PARAM    = 0x14
 
-# Module state codes - states specific for AceHigh modules 
-ACE_STATE_STANDBY      = 0x01  # Module is idle not executing any functions, ready for commands
-ACE_STATE_FAULT        = 0x02  # Module is in fault state
-ACE_STATE_EXECUTING    = 0x03  # Module is executing functions, but can receive new commands
-ACE_STATE_BOOTLOADER   = 0x04  # Module is in bootloader mode
+rx_queue = queue.Queue()
 
 CHANNEL = "/dev/cu.usbmodem2080317458421"
 BITRATE = 1000000
@@ -61,11 +58,11 @@ def safe_print(*args, **kwargs):
 
 def status_to_string(status: int) -> str:
     lookup = {
-        STATUS_OK: "Accepted, status OK",
-        STATUS_QUEUED: "Accepted, queued",
-        STATUS_DATA_FOLLOWS: "Accepted, data follows",
-        STATUS_UNKNOWN_COMMAND: "Unknown command",
-        STATUS_INVALID_PARAM: "Invalid parameter",
+        ACE_STATUS_OK: "Accepted, status OK",
+        ACE_STATUS_QUEUED: "Accepted, queued",
+        ACE_STATUS_DATA_FOLLOWS: "Accepted, data follows",
+        ACE_STATUS_UNKNOWN_COMMAND: "Unknown command",
+        ACE_STATUS_UNKNOWN_PARAM: "Invalid parameter",
     }
     return lookup.get(status, f"Unknown status 0x{status:02X}")
 
@@ -80,19 +77,13 @@ def update_can_ids() -> None:
 
 # START build custom commands -----------------------------------------------------------------------------------------
 
-# BOOTLOADER commands START: ------------------------------------------------------------------------------------------
-#   1. Send BOOT_PING
-#   2. Check response - error handling
-#   3. Send BOOT_START , size = 12 for test
-#   4. Check payload[0] == 1
-#   5. send BOOT_DATA seq 0, bytes 0x11 0x22 0x33 0x44
-#   6. send BOOT_DATA seq 1, bytes 0x55 0x66 0x77 0x88
-#   7. send BOOT_DATA seq 2, bytes 0x99 0xAA 0xBB 0xCC
-#   8. send BOOT_END
-#   9. Expect jump to application
-
-
-# BOOTLOADER commands END: ------------------------------------------------------------------------------------------
+def build_reset() -> can.Message:
+    data = [ACE_CMD_WRITE, ACE_PARAM_BOOT, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
+    return can.Message(
+        arbitration_id=ACE_CAN_ID_COMMAND,
+        is_extended_id=False,
+        data=data
+    )
 
 def build_read_led() -> can.Message:
     data = [ACE_CMD_READ, RANGER_PARAM_LED_PA1, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
@@ -146,7 +137,6 @@ def build_read_node_id() -> can.Message:
 
 # END build custom commands --------------------------------------------------------------------------------
 
-
 def print_message(prefix: str, msg: can.Message) -> None:
     hex_data = " ".join(f"{b:02X}" for b in msg.data)
     safe_print(f"{prefix} ID=0x{msg.arbitration_id:03X} DLC={msg.dlc} DATA=[{hex_data}]")
@@ -162,43 +152,26 @@ def decode_response(msg: can.Message) -> None:
     status_code  = msg.data[2]
     payload      = msg.data[3:8]
 
-    print_message("Response", msg)
-    safe_print(f"  command_id   = 0x{command_id:02X}")
-    safe_print(f"  parameter_id = 0x{parameter_id:02X}")
-    safe_print(f"  status_code  = 0x{status_code:02X} ({status_to_string(status_code)})")
-    safe_print(f"  payload      = {[f'0x{x:02X}' for x in payload]}")
+    if (command_id != ACE_CMD_BOOT_DATA):
+        print_message("Response", msg)
+        safe_print(f"  command_id   = 0x{command_id:02X}")
+        safe_print(f"  parameter_id = 0x{parameter_id:02X}")
+        safe_print(f"  status_code  = 0x{status_code:02X} ({status_to_string(status_code)})")
+        safe_print(f"  payload      = {[f'0x{x:02X}' for x in payload]}")
 
-    if command_id == ACE_CMD_READ and status_code == STATUS_DATA_FOLLOWS and parameter_id == RANGER_PARAM_LED_PA1:
+    if command_id == ACE_CMD_READ and status_code == ACE_STATUS_DATA_FOLLOWS and parameter_id == RANGER_PARAM_LED_PA1:
         led_state = payload[0]
-        safe_print(f"  decoded LED state = {'OFF' if led_state else 'ON'}")
+        safe_print(f" decoded LED state = {'OFF' if led_state else 'ON'}")
 
-    if command_id == ACE_CMD_READ and status_code == STATUS_DATA_FOLLOWS and parameter_id == RANGER_PARAM_NODE_ID:
+    if command_id == ACE_CMD_READ and status_code == ACE_STATUS_DATA_FOLLOWS and parameter_id == RANGER_PARAM_NODE_ID:
         node_id = payload[0]
-        safe_print(f"  decoded Node ID = 0x{node_id:02X} ({node_id})")
+        safe_print(f" decoded Node ID = 0x{node_id:02X} ({node_id})")
 
     if command_id == ACE_CMD_WRITE and parameter_id == RANGER_PARAM_NODE_ID:
-        if status_code == STATUS_OK:
+        if status_code == ACE_STATUS_OK:
             safe_print("  Node ID write accepted")
         else:
             safe_print("  Node ID write failed")
-    
-    if command_id == ACE_CMD_BOOT_PING and parameter_id == ACE_PARAM_BOOT:
-        safe_print("  BOOTPING reply received")
-
-        protocol_version = payload[0]
-        bootloader_version = payload[1]
-        module_state = payload[2]
-
-        safe_print(f"  Protocol version = 0x{protocol_version:02X}")
-        safe_print(f"  Bootloader version = 0x{bootloader_version:02X}")
-        safe_print(f"  Module state = 0x{module_state:02X}")
-        if status_code == STATUS_OK:
-            safe_print("  Module alive")
-        if module_state == ACE_STATE_BOOTLOADER:
-            safe_print("  Module is in BOOTLOAD loop")
-    
-    if command_id == ACE_CMD_BOOT_DATA and parameter_id == ACE_PARAM_BOOT:
-        variable = 0 # Do nothing
 
 
 def listener_thread(bus: can.Bus) -> None:
@@ -215,14 +188,25 @@ def listener_thread(bus: can.Bus) -> None:
                 print_message("Heartbeat", msg)
             elif msg.arbitration_id == ACE_CAN_ID_RESPONSE:
                 decode_response(msg)
+                rx_queue.put(msg) # Only put ACE_CAN_ID_RESPONSE messages into the queue
             else:
                 print_message("Other", msg)
-
+            
         except Exception as exc:
             safe_print(f"Listener error: {exc}")
             break
 
     safe_print("Background listener stopped.")
+
+
+def wait_for_response(timeout=1.0):
+
+    try:
+        msg = rx_queue.get(timeout=timeout)
+        return msg
+
+    except queue.Empty:
+        return None
 
 
 def send_command(bus: can.Bus, msg: can.Message, label: str) -> None:
@@ -248,20 +232,23 @@ def print_menu() -> None:
     safe_print("  m  -> show menu")
     safe_print("  q  -> quit\n")
 
+# START Program new Firmware  --------------------------------------------------------------------------------
 
 def program_new_firmware(bus: can.Bus):
 
     safe_print("Starting firmware update...")
 
-    # Load firmware .bin file 
-    parser = argparse.ArgumentParser()
-    parser.add_argument("firmware", help="Path to firmware .bin file")
-    args = parser.parse_args()
+    firmware_path = "/Users/tor/Documents/Ranger/Ranger_module_firmware/ranger_mk1_v2/Debug/ranger_mk1_v2.bin"
 
-    with open(args.firmware, "rb") as f:
+    with open(firmware_path, "rb") as f:
         firmware = f.read()
 
-    # Send _BOOT_PING command - for now CAN listener is running in the background
+    fw_size = len(firmware)
+    safe_print(f"Firmware file: {firmware_path}")
+    safe_print(f"Firmware size: {fw_size} bytes / 0x{fw_size:08X}")
+
+    # _BOOT_PING command
+
     data = [ACE_CMD_BOOT_PING, ACE_PARAM_BOOT, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00]
     
     can_message = can.Message(
@@ -270,11 +257,11 @@ def program_new_firmware(bus: can.Bus):
         data=data
     )
     bus.send(can_message)
-    time.sleep(0.1)
+    response_msg = wait_for_response(timeout=2.0)
 
-    # Send _BOOT_START command
-    fw_size = len(firmware)
-
+    # Print received frame with Ace protocol version and module bootloader version before continuing
+    
+    # _BOOT_START command
     data = [ACE_CMD_BOOT_START,
         ACE_PARAM_BOOT,
         0x00,
@@ -283,15 +270,14 @@ def program_new_firmware(bus: can.Bus):
         (fw_size >> 16) & 0xFF,
         (fw_size >> 24) & 0xFF,
         0x00]
-    
+
     can_message = can.Message(
         arbitration_id=ACE_CAN_ID_COMMAND,
         is_extended_id=False,
         data=data
     )
     bus.send(can_message)
-    time.sleep(6)
-
+    response_msg = wait_for_response(timeout=10.0)
 
     # _BOOT_DATA
     # Transfer firmware in chunks of 4 bytes per CAN frame
@@ -303,25 +289,57 @@ def program_new_firmware(bus: can.Bus):
     for offset in range(0, len(firmware), BYTES_PER_LINE):
         fw_array = firmware[offset:offset + BYTES_PER_LINE]
 
-        safe_print("Transferring data")
-    
         seq_LSB = seq_counter & 0xFF
         seq_MSB = (seq_counter >> 8) & 0xFF
 
-        while len(fw_array) < 4: # pad the frame if bytes are less than 4
+        while len(fw_array) < 4:  # pad the frame if bytes are less than 4
             fw_array += b"\xFF"   # Flash erased state padding
-
         
         data = [ACE_CMD_BOOT_DATA, ACE_PARAM_BOOT, seq_LSB, seq_MSB, fw_array[0], fw_array[1], fw_array[2], fw_array[3]]
+        
+        #safe_print("Transferring data")
+        #safe_print(f"TX: {' '.join(f'{b:02X}' for b in data)}")
+
         can_message = can.Message(
              arbitration_id=ACE_CAN_ID_COMMAND,
              is_extended_id=False,
              data=data
          )
         bus.send(can_message)
-        seq_counter = seq_counter + 1
-        print(f"TX: {' '.join(f'{b:02X}' for b in data)}")
-        time.sleep(0.01)
+
+        response_msg = wait_for_response(timeout=10.0)
+        
+        if response_msg is None:
+            safe_print("BOOT_DATA timeout")
+            return
+
+        if(response_msg.data[3] != ACE.STATE_BOOTLOADER_ACTIVE):
+            # The bootloader did not return ACE_STATE_BOOTLOADER_ACTIVE so a fault occoured
+            safe_print("BOOT_DATA did not complete correctly: Bootloader not active: ", response_msg)
+
+            if(response_msg.data[3] == ACE.STATE_FIRMWARE_SEQUENCE_FAULT):
+                safe_print("CAN frame firmware sequence error: ", response_msg)
+                return
+
+            if(response_msg.data[3] == ACE.STATE_FIRMWARE_ERASE_FAULT):
+                safe_print("Module flash erase fault: ", response_msg)
+                return            
+
+            if(response_msg.data[3] == ACE.STATE_FIRMWARE_WRITE_FAULT):
+                safe_print("Module flash write fault: ", response_msg)
+                return            
+
+            if(response_msg.data[3] == ACE.STATE_FIRMWARE_SIZE_FAULT):
+                safe_print("Firmware size miss match: ", response_msg)
+                return   
+
+        seq_counter = seq_counter + 1 # increment sequence counter for next CAN frame 
+        
+        if (seq_counter % 100 == 0): # a little GUI loading firmware bar
+            print("/", end="")
+        
+    print("")
+    time.sleep(0.1)
 
     # _BOOT_END
     # Signal end the Firmware transfer from the host (module should jump to the application automatically)
@@ -333,9 +351,19 @@ def program_new_firmware(bus: can.Bus):
         data=data
     )
     bus.send(can_message)
+    response_msg = wait_for_response(timeout=10.0)
 
-    safe_print("Transfer complete!")
-    safe_print("Jumping to application")
+    if response_msg is None:
+        safe_print("BOOT_END timeout")
+        return
+
+    if(response_msg.data[3] == ACE.STATE_APP_VALID):
+        # Application was marked as valid by the MCU
+        safe_print("Transfer complete, jumping to application")
+
+
+
+# END Program new Firmware --------------------------------------------------------------------------------
 
 # Main loop definition STARTS here -------------------------------------------------------------------------------------
 
@@ -402,11 +430,11 @@ def main() -> None:
                     except ValueError:
                         safe_print("Invalid input")
 
-                elif cmd == "b":
-                    send_command(bus, build_boot_ping(), "SEND BOOTLOADER PING")
-
                 elif cmd == "p":
                     program_new_firmware(bus);
+
+                elif cmd == "r":
+                    send_command(bus, build_reset(), "Software reset module")
 
                 elif cmd == "m":
                     print_menu()
---
+++ Python/host/ace_constants.py
@@ -0,0 +1,13 @@
+
+
+# Module state codes - states specific for AceHigh modules and present in Ace protocol files
+STATE_STANDBY           		= 0x21
+STATE_FAULT             		= 0x22
+STATE_EXECUTING         		= 0x23
+STATE_APP_VALID					= 0x24
+STATE_BOOTLOADER_ACTIVE     	= 0x25
+STATE_BOOTLOADER_INACTIVE   	= 0x26
+STATE_FIRMWARE_SIZE_FAULT   	= 0x27
+STATE_FIRMWARE_ERASE_FAULT  	= 0x28
+STATE_FIRMWARE_SEQUENCE_FAULT  	= 0x29
+STATE_FIRMWARE_WRITE_FAULT  	= 0x2A
